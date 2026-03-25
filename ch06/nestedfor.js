9d8a69837ad4d54e38a85ab9dc4bf640caf83e2c
inbois2010-star <inbois2010@gmail.com>
2026-03-25 11:25:50 +0900

    반복문을 이용한 곱셈

---
+++ ch06/nestedfor.js
@@ -0,0 +1,3 @@
+for (let i=1;i<=9;i++){
+    for (let e=1;e<=9;e++){console.log(`${i} X ${e}`)}
+}
---
+++ ch06/nestedwhile.js
@@ -0,0 +1,11 @@
+let i=1;
+let e=1;
+while(i < 10){
+    e = 1;
+    while(e < 10){
+        console.log(`${i} X ${e} = ${i*e}`);
+        e++;    
+    }
+    i++;
+    console.log("\n")
+}
