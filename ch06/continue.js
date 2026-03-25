bc10c898a8ff7dce34c12a150aceb37de89c4f22
inbois2010-star <inbois2010@gmail.com>
2026-03-25 10:26:16 +0900

    반복문

---
+++ ch06/continue.js
@@ -0,0 +1,6 @@
+let i=1;
+while (i<10){
+    i++;
+    if ((i%2)==0) continue;
+    console.log(i)
+}
---
+++ ch06/while.js
@@ -0,0 +1,6 @@
+let month=1;
+while (1){
+    console.log(month+"월");
+    if (month>=12){break;}
+    month++;
+}
